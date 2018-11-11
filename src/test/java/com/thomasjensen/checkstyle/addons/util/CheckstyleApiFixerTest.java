e8bdfd2f8b9c24685782213a205dfd7b21b94674
Thomas Jensen <tsjensen@users.noreply.github.com>
2018-11-11 14:17:14 +0100

    Add header to new test file

--- src/test/java/com/thomasjensen/checkstyle/addons/util/CheckstyleApiFixerTest.java
+++ src/test/java/com/thomasjensen/checkstyle/addons/util/CheckstyleApiFixerTest.java
@@ -1,4 +1,19 @@
 package com.thomasjensen.checkstyle.addons.util;
+/*
+ * Checkstyle-Addons - Additional Checkstyle checks
+ * Copyright (C) 2015 Thomas Jensen
+ *
+ * This program is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License, version 3, as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this
+ * program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 import org.junit.Assert;
